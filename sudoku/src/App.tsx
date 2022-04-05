01959dbc02fd4de02cc8543edb32ab6fa232276c
Nadja <92398598+NadjaGrothe@users.noreply.github.com>
2022-04-05 07:57:08 +0100

    added key value to square and initiated empty arrays for board state

--- sudoku/src/App.tsx
+++ sudoku/src/App.tsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Square from "./components/Board/Square";
 
 function App() {
+   const [board, setBoard] = useState<number[][]>(
+      Array(9).fill(Array(9).fill(0))
+   );
+
+   console.log(board);
+
    return (
       <body className="App">
          <header className="App-header">
             <h1>Sudoku</h1>
          </header>
          <main className="App-main">
-            <Square value={123} isDisabled={true}></Square>
+            <Square key={1} value={123} isDisabled={true}></Square>
             <Square
+               key={2}
                value={undefined}
                isDisabled={false}
                onChange={(e) => console.log(e.target.value)}
--- sudoku/src/components/Board/Square/index.tsx
+++ sudoku/src/components/Board/Square/index.tsx
@@ -2,20 +2,23 @@ import React from "react";
 import "./Square.style.css";
 
 export interface SquareProps {
+   key: number
    value?: number | undefined;
    isDisabled: boolean;
    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function Square({ value, isDisabled, onChange }: SquareProps) {
+function Square({ key, value, isDisabled, onChange }: SquareProps) {
    return isDisabled ? (
       <input
+         key={key}
          className="Board-square"
          value={value}
          disabled={isDisabled}
       ></input>
    ) : (
       <input
+         key={key}
          className="Board-square"
          type="number"
          defaultValue={value}
